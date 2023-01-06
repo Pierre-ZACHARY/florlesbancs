2413234e47863e01b47a6c804ddc28dbcdcb9c4c
Eoghann VEAUTE <eoghann.veaute@etu.univ-orleans.fr>
2023-01-06 19:29:18 +0100

    affichage des layers sur la carte

--- src/Components/Context/GeoServerContext.tsx
+++ src/Components/Context/GeoServerContext.tsx
@@ -1,5 +1,8 @@
-import {createContext, PropsWithChildren, useState} from "react";
-import {GeoServerData} from "../../utils/fetchGeoserverData";
+import {createContext, PropsWithChildren, useContext, useEffect, useState} from "react";
+import fetchGeoserverData, {GeoServerData} from "../../utils/fetchGeoserverData";
+import TileLayer from "ol/layer/Tile";
+import {TileWMS} from "ol/source";
+import {MapContext} from "./MapContainer";
 
 
 interface GeoServerContextType { // TODO c'est juste un exemple de context, à toi de voir comment tu veux faire
@@ -7,23 +10,39 @@ interface GeoServerContextType { // TODO c'est juste un exemple de context, à t
     filter: string; // TODO : Change to enum
 }
 
-export const GeoServerContext = createContext<GeoServerContextType | null>(null);
+export const GeoServerContext = createContext<TileWMS[] | null>(null);
 
 
 export default function GeoServerContextComponent(props: PropsWithChildren<{}>) {
 
     // /!\ ne jamais fetch un objet à ce niveau, sinon il sera refetch à chaque re-render
 
-    const [context] = useState<GeoServerContextType | null>( () => {
-        // TODO ici on fetch les données via le utils dans fetchGeoserverData.ts
-        // et il suffit de les return en dessous ( en respectant le type définit plus haut )
-        return {data: null, filter: "all"}
-    });
+    // const [context] = useState<GeoServerContextType | null>( () => {
+    //     // TODO ici on fetch les données via le utils dans fetchGeoserverData.ts
 
+    const [data,setData] = useState<TileWMS[] | null>( null );
+    useEffect(() => {
+        fetchGeoserverData().then((data) => setData(data))}, []);
+    //var layers = [new TileLayer({source:data[0]}), new TileLayer({source:data[1]})]
 
+    const map = useContext(MapContext);
+    useEffect ( ()=>{
+        if (data) {
+            const espaces = new TileLayer({source: data[0]});
+            const pav = new TileLayer({source: data[1]});
+
+            map?.addLayer(espaces);
+            map?.addLayer(pav);
+
+            return () => {
+                map?.removeLayer(espaces);
+                map?.removeLayer(pav);
+            }
+        }
+    }, [data])
 
     return (
-        <GeoServerContext.Provider value={context}>
+        <GeoServerContext.Provider value={data}>
             {props.children}
         </GeoServerContext.Provider>
     )
