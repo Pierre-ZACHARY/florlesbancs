595879b5a6269801f88616fab30c1c14b76aec14
Eoghann VEAUTE <eoghann.veaute@etu.univ-orleans.fr>
2023-01-07 16:07:28 +0100

    changement des TileLayer en VectorLayer (source aussi) + fonction pour avoir icone

--- src/Components/Context/GeoServerContext.tsx
+++ src/Components/Context/GeoServerContext.tsx
@@ -1,8 +1,16 @@
 import {createContext, PropsWithChildren, useContext, useEffect, useState} from "react";
 import fetchGeoserverData, {GeoServerData} from "../../utils/fetchGeoserverData";
 import TileLayer from "ol/layer/Tile";
-import {TileWMS} from "ol/source";
+import {TileWMS, Vector} from "ol/source";
 import {MapContext} from "./MapContainer";
+import VectorLayer from "ol/layer/Vector";
+import {Feature} from "ol";
+import LocationMarker from "../Markers/LocationMarker";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChair, faLocationPin, faMonument} from "@fortawesome/free-solid-svg-icons";
+import {faPagelines} from "@fortawesome/free-brands-svg-icons";
+import {Geometry, Point} from "ol/geom";
+import {Coordinate} from "ol/coordinate";
 
 
 interface GeoServerContextType { // TODO c'est juste un exemple de context, à toi de voir comment tu veux faire
@@ -10,8 +18,24 @@ interface GeoServerContextType { // TODO c'est juste un exemple de context, à t
     filter: string; // TODO : Change to enum
 }
 
-export const GeoServerContext = createContext<TileWMS[] | null>(null);
+export const GeoServerContext = createContext<Vector[] | null>(null);
 
+function changeIcon(feat: Feature) {
+    var descr = feat.getProperties()["descriptio"];
+    switch (descr) {
+        case "Banc public":
+            return <FontAwesomeIcon icon={faChair}/>
+
+        case "Colonne végétale":
+            return <FontAwesomeIcon icon={faPagelines}/>
+
+        case "Statue, monument" :
+            return <FontAwesomeIcon icon={faMonument}/>
+
+        default:
+            return <FontAwesomeIcon icon={faLocationPin}/>
+    }
+}
 
 export default function GeoServerContextComponent(props: PropsWithChildren<{}>) {
 
@@ -20,16 +44,30 @@ export default function GeoServerContextComponent(props: PropsWithChildren<{}>) 
     // const [context] = useState<GeoServerContextType | null>( () => {
     //     // TODO ici on fetch les données via le utils dans fetchGeoserverData.ts
 
-    const [data,setData] = useState<TileWMS[] | null>( null );
+    const [data, setData] = useState<Vector[] | null>(null);
+    const [layers, setLayers] = useState<VectorLayer<any>[]>();
     useEffect(() => {
-        fetchGeoserverData().then((data) => setData(data))}, []);
-    //var layers = [new TileLayer({source:data[0]}), new TileLayer({source:data[1]})]
+        fetchGeoserverData().then((data) => setData(data))
+    }, []);
 
     const map = useContext(MapContext);
-    useEffect ( ()=>{
+    useEffect(() => {
         if (data) {
-            const espaces = new TileLayer({source: data[0]});
-            const pav = new TileLayer({source: data[1]});
+            const espaces = new VectorLayer({source: data[0]});
+            const pav = new VectorLayer({source: data[1]});
+
+            // @ts-ignore
+            setLayers([espaces, pav]);
+            const es_s = espaces.getSource();
+
+            // @ts-ignore
+            es_s.on('change', function(evt){
+                var source=evt.target;
+                if(source.getState() === 'ready'){
+                    // @ts-ignore
+                    console.log(es_s.getFeatures()[0].getGeometry()["flatCoordinates"]);
+                }
+            });
 
             map?.addLayer(espaces);
             map?.addLayer(pav);
@@ -43,6 +81,12 @@ export default function GeoServerContextComponent(props: PropsWithChildren<{}>) 
 
     return (
         <GeoServerContext.Provider value={data}>
+            {/*{layers[0].getSource().getFeatures()?.map((feature) => <LocationMarker position={feature.getGeometry()["flatCoordinates"]} onClick={() => {}} positioning={"center-center"}>*/}
+            {/*    {changeIcon(feature)}*/}
+            {/*</LocationMarker>)}*/}
+            {/*{layers[1].getSource().getFeatures()?.map( (feature) => <LocationMarker position={feature.getGeometry()["flatCoordinates"]} onClick={() => {}} positioning={"center-center"}>*/}
+            {/*    {changeIcon(feature)}*/}
+            {/*</LocationMarker>)}*/}
             {props.children}
         </GeoServerContext.Provider>
     )
--- src/utils/fetchGeoserverData.ts
+++ src/utils/fetchGeoserverData.ts
@@ -1,5 +1,8 @@
 import {TileWMS} from "ol/source";
 import {Point} from "ol/geom";
+import {Vector} from "ol/source";
+import {GeoJSON} from "ol/format";
+import {log} from "util";
 
 
 export abstract class GeoServerData{
@@ -8,19 +11,21 @@ export abstract class GeoServerData{
 }
 
 // TODO créer une fonction qui fait une requête à geoserver et retourne toutes les info sur les structures d'Orléans
-export default async function fetchGeoserverData(): Promise<TileWMS[]> {
+export default async function fetchGeoserverData(): Promise<Vector[]> {
 
     // pour un exemple de requête voir findNearestBench
     // /!\ faire très attention avec le système de coordonnées de geoserver ( je sais pas quel système il utilise, souvent c'est EPSG 3857. Il y a la fonction fromLonLat qui permet de convertir ) et celui de openlayers WGS 84 (EPSG:4326)
-    var espaces = new TileWMS({
-            url: 'http://localhost:8080/geoserver/toto/wms',
-            params: {'LAYERS': 'toto:espaces_verts_voirie'},
-            serverType: 'geoserver'
+    var espaces = new Vector({
+        url: 'http://localhost:8080/geoserver/toto/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=toto%3Aespaces_verts_voirie&outputFormat=application%2Fjson',
+        format: new GeoJSON(),
+
         });
-    var pav = new TileWMS({
-            url: 'http://localhost:8080/geoserver/toto/wms',
-            params: {'LAYERS': 'toto:dechets_pav'},
-            serverType: 'geoserver'
+    var pav = new Vector({
+            url: 'http://localhost:8080/geoserver/toto/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=toto%3Adechets_pav&outputFormat=application%2Fjson',
+            format: new GeoJSON(),
         });
+
+    await console.log(espaces);
+
     return [espaces, pav] // TODO remplir la liste
 }
