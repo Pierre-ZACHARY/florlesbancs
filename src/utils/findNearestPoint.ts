c931dd0ff8390aeac03fd2964144d5228f5c20ef
Eoghann VEAUTE <eoghann.veaute@etu.univ-orleans.fr>
2023-01-08 17:26:35 +0100

    changement de la fonction pour renvoyer le point le + proche (marge)

--- src/utils/findNearestPoint.ts
+++ src/utils/findNearestPoint.ts
@@ -1,32 +1,46 @@
 import {Coordinate} from "ol/coordinate";
 import {Feature} from "ol";
-import {Map} from "ol"
-import {Pixel} from "ol/pixel";
+import VectorLayer from "ol/layer/Vector";
 
-export default function findNearestPointOnVectorLayer(map : Map, pixel : Pixel, coords: Coordinate) : Feature {
+
+
+export default function findNearestPointOnVectorLayer(vertsLayers : VectorLayer<any>[], pavLayers : VectorLayer<any>[], coords: Coordinate) : Feature {
     let minDistance = Infinity;
     let nearestCoords;
     let nearestFeature;
-    console.log("aaa");
-    map.forEachFeatureAtPixel(pixel, (feature) => {
-        console.log(feature);
-        // @ts-ignore
+
+    for (let i = 0; i < vertsLayers.length; i++){
+        console.log(i)
+        const feature = vertsLayers[i].getSource().getClosestFeatureToCoordinate(coords);
         const featureCoordinates = feature.getGeometry().getCoordinates();
         const distance = calculateDistance(coords, featureCoordinates);
         if (distance < minDistance) {
+            console.log("SWITCH");
             minDistance = distance;
             nearestCoords = featureCoordinates;
             nearestFeature = feature;
         }
-    });
+    }
+
+    for (let i = 0; i < pavLayers.length; i++){
+        console.log(i)
+        const feature = pavLayers[i].getSource().getClosestFeatureToCoordinate(coords);
+        const featureCoordinates = feature.getGeometry().getCoordinates();
+        const distance = calculateDistance(coords, featureCoordinates);
+        if (distance < minDistance) {
+            console.log("SWITCH");
+            minDistance = distance;
+            nearestCoords = featureCoordinates;
+            nearestFeature = feature;
+        }
+    }
+
     // @ts-ignore
     return nearestFeature;
 
 }
 function calculateDistance(point1:Coordinate, point2: Coordinate){
-    // Use the Haversine formula to calculate the distance between two points on a sphere
-    // More info: https://en.wikipedia.org/wiki/Haversine_formula
-    const R = 6371e3; // Earth's radius in meters
+    const R = 6371e3;
     const phi1 = point1[1] * Math.PI / 180;
     const phi2 = point2[1] * Math.PI / 180;
     const deltaPhi = (point2[1] - point1[1]) * Math.PI / 180;
